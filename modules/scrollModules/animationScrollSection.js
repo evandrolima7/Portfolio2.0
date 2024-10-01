fc8dd34d5a4fd8d3a4ba762098307d96a898a2f4
.browserslistrcEvandro <evandrodejesuslima@gmail.com>
2024-09-30 21:34:10 -0300

    footer size

--- modules/scrollModules/animationScrollSection.js
+++ modules/scrollModules/animationScrollSection.js
@@ -1,6 +1,6 @@
 export default function animationScrollSection(){
     const sections = document.querySelectorAll('.js-scroll');
-    const metadeWindow = window.innerHeight * 0.35;
+    const metadeWindow = window.innerHeight * 0.28;
 
     function scrollSection(){
         sections.forEach((section) => {
