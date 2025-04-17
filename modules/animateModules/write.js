9abc7e46008cb738fc477ae2aea87438078ab3b3
.browserslistrcEvandro <evandrodejesuslima@gmail.com>
2025-04-17 00:13:55 -0300

    atualizando projetos praticos

--- modules/animateModules/write.js
+++ modules/animateModules/write.js
@@ -3,35 +3,35 @@ export default function initAnimaWrite() {
     const welcomeMessage = document.getElementById("welcomeMessage");
     let index = 0;
     let isDeleting = false;
-    let typingSpeed = 150; // Velocidade de digitação inicial
+    let typingSpeed = 150;
   
     function typeWriter() {
-      // Se estiver apagando, reduz o tamanho da string mostrada
+      
       if (isDeleting) {
         welcomeMessage.innerHTML = text.substring(0, index - 1);
         index--;
         typingSpeed = 50; // Velocidade mais rápida ao apagar
       } 
-      // Se estiver digitando, aumenta o tamanho da string mostrada
+      
       else {
         welcomeMessage.innerHTML = text.substring(0, index + 1);
         index++;
-        typingSpeed = 150; // Velocidade normal ao digitar
+        typingSpeed = 150; 
       }
   
-      // Quando o texto completo foi digitado
+      
       if (!isDeleting && index === text.length) {
-        isDeleting = true; // Começa a apagar
-        typingSpeed = 1000; // Pausa por 1 segundo antes de começar a apagar
+        isDeleting = true;
+        typingSpeed = 1000; 
       } 
-      // Quando o texto foi completamente apagado
+      
       else if (isDeleting && index === 0) {
-        isDeleting = false; // Volta a digitar
-        typingSpeed = 150; // Reinicia a velocidade de digitação
+        isDeleting = false; 
+        typingSpeed = 150; 
       }
   
-      setTimeout(typeWriter, typingSpeed); // Controla o intervalo entre os caracteres
+      setTimeout(typeWriter, typingSpeed); 
     }
   
-    typeWriter(); // Inicia o efeito de digitação
+    typeWriter();
   }
